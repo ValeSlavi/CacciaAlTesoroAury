0bc532a1286d98e267ffe4f2985f4054923265c6
Valentina_PC <valentina.slaviero@edu-its.it>
2025-07-11 19:58:31 +0200

    Add Login Iniziale

---
+++ src/main/java/com/aurorina/tesoro/controller/LoginInizialeController.java
@@ -0,0 +1,34 @@
+package com.aurorina.tesoro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.aurorina.tesoro.entity.User;
+import com.aurorina.tesoro.service.UserService;
+
+import org.springframework.ui.Model;
+
+public class LoginInizialeController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/loginIniziale")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @PostMapping("/loginIniziale")
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
+                        Model model) {
+        User user = userService.authenticate(username, password);
+        if (user != null) {
+            return "redirect:/";
+        } else {
+            model.addAttribute("error", "Credenziali non valide");
+            return "login";
+        }
+    }
+}

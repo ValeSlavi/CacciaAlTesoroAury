1cb6126d7150c0df4989cab5a0d2ce402716416f
Valentina_PC <valentina.slaviero@edu-its.it>
2025-07-11 19:34:43 +0200

    Add little part in application.proprieties and insert schema and data fictius

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password) VALUES ('BadGirl', 'BuonCompleanno!');
+INSERT INTO users (username, password) VALUES ('', 'flag{HaiRisoltoLEnigma}');
+INSERT INTO users (username, password) VALUES ('', 'flag{}');
+INSERT INTO users (username, password) VALUES ('', 'flag{}');

4ff01adfd9e4a468130f7747ad1be9795a1647c5
Valentina_PC <valentina.slaviero@edu-its.it>
2025-07-11 18:42:07 +0200

    Primo commit

---
+++ src/test/java/com/aurorina/tesoro/TesoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurorina.tesoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
